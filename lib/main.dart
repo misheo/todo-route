f5d25418f67dc263b213ef676053baa3d094f45e
Said Nassar <saidnassar18@gmail.com>
2024-07-27 17:57:29 +0200

    Add Firebase Integration and FlutterFire Configuration
    This commit integrates Firebase into the Flutter project, enabling core Firebase functionalities. It includes the addition of Firebase dependencies in `pubspec.yaml`, configuration files for Firebase services, and necessary Gradle plugins for Android. The changes encompass:
    
    - Adding Firebase Core dependency and FlutterFire configuration in `pubspec.yaml`.
    - Including `google-services.json` for Firebase Android configuration.
    - Modifying `build.gradle` files to apply Google Services plugin.
    - Generating and adding `firebase_options.dart` for platform-specific Firebase configurations.
    - Updating `main.dart` to initialize Firebase with the generated options.
    - Registering Firebase Core plugin in platform-specific plugin registrants for macOS and Windows.
    - Adding FlutterFire CLI configuration in `firebase.json`.
    - Including FlutterFlavorizr for potential future flavor configuration in `pubspec.yaml`.
    - Updating `pubspec.lock` to reflect new dependencies.
    
    These changes lay the foundation for leveraging Firebase services within the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo/firebase_options.dart';
 import 'package:todo/todo_app.dart';
 
 import 'core/helper/cash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await Cash().init();
+  await Cash().init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const TodoApp());
 }
