954553db282cb9510e9f677a5a0f2fe6fba1e27e
Said Nassar <saidnassar18@gmail.com>
2024-08-06 16:58:33 +0300

    Add Firebase Authentication for Task Retrieval
    Integrate Firebase Authentication to ensure tasks are retrieved based on the
    currently authenticated user. This change modifies the `getTasksFromFirebase`
    method to use `FirebaseAuth.instance.currentUser` for filtering tasks by user ID,
    enhancing security and personalization of task data. If no user is authenticated,
    the method now returns an empty list, ensuring that only authorized user data is
    accessed.

--- lib/core/networking/firebase_api.dart
+++ lib/core/networking/firebase_api.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../models/task.dart';
 
@@ -10,19 +11,26 @@ class FirebaseApi {
   }
 
   static Future<void> addTaskToFirebase(Task task) async {
-   var taskCollection = getTaskCollection();
-    DocumentReference<Task> tasksRef = taskCollection.doc(); 
+    var taskCollection = getTaskCollection();
+    DocumentReference<Task> tasksRef = taskCollection.doc();
     task.id = tasksRef.id;
     await tasksRef.set(task);
   }
 
   static Future<List<Task>> getTasksFromFirebase() async {
-    //get all tasks from firebase when the user id is equel to user id in firebase
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    var taskCollection = firestore.collection('tasks').withConverter<Task>(
-        fromFirestore: ((snapshot, options) => Task.fromJson(snapshot.data()!)),
-        toFirestore: (task, options) => task.toJson()).where('user_id' , isEqualTo: 'user_id');
-    var querySnapshot = await taskCollection.get();
-    return querySnapshot.docs.map((doc) => doc.data()).toList();
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      var taskCollection = firestore
+          .collection('tasks')
+          .withConverter<Task>(
+              fromFirestore: ((snapshot, options) =>
+                  Task.fromJson(snapshot.data()!)),
+              toFirestore: (task, options) => task.toJson())
+          .where("user_id", isEqualTo: user.uid);
+      var querySnapshot = await taskCollection.get();
+      return querySnapshot.docs.map((doc) => doc.data()).toList();
+    }
+    return [];
   }
 }
